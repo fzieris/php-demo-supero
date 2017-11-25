7e3f3decede1028489fba6c6890770603f5b20c4
Franz Zieris <franz.zieris@fu-berlin.de>
2017-11-25 13:14:48 +0100

    Nutze Twig-Templates für HTML-Ausgabe
    Die gemeinsam genutzte neue Datei `bootstrap.php` beinhaltet die
    Twig-Konfiguration.

--- web/held.php
+++ web/held.php
@@ -1,69 +1,43 @@
-<!DOCTYPE html>
-<html lang="de">
-<head>
-    <meta charset="utf-8">
-    <?php
-    // direkter Zugriff auf $_GET['name'] ginge auch;
-    // filter_input() erspart aber u.a. die Prüfung isset($_GET['name'])
-    $name = filter_input(INPUT_GET, 'name');
+<?php
+// Gemeinsame genutzte Dinge sind dorthin ausgelagert
+require_once '../bootstrap.php';
 
-    $heros = array(
-        'Batman' => array(
-            'price' => "100 €/h",
-            'status' => "ausgebucht",
-            'text' => "Nachtaktiv und günstig im Doppelpack mit Robin. "
-                    . "Gutes Gehör für hohe Töne, singt im Heldenchor aber im Bass."),
-        'Flash' => array(
-            'price' => "10 €/sec",
-            'status' => "fast ausgebucht",
-            'text' => "Ihr Held für eilige Einsätze. "
-                   . "Jetzt neu: Sekundengenaue Abrechnung für Ihren Auftrag."),
-        'Robin' => array(
-            'price' => "50 €/h",
-            'status' => "verfügbar",
-            'text' => "Jung und dynamisch. Guter Freund von Batman. "
-                   . "Trägt gerne Strumpfhosen, vor allem im Winter."),
-    );
-    $hero = $heros[$name];
+// direkter Zugriff auf $_GET['name'] ginge auch;
+// filter_input() erspart aber u.a. die Prüfung isset($_GET['name'])
+$name = filter_input(INPUT_GET, 'name');
 
-    // Bestimmen der CSS-Klasse anhand der Helden-Verfügbarkeit
-    $statusClasses = array(
-        'verfügbar' => 'success',
-        'fast ausgebucht' => 'warning',
-        'ausgebucht' => 'danger',
-    );
-    $class = $statusClasses[$hero['status']];
-    ?>
-    <title>Supero - Held <?=$name?></title>
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/bootstrap-theme.min.css">
-</head>
-<body>
-<div class="container">
+$heros = array(
+    'Batman' => array(
+        'price' => "100 €/h",
+        'status' => "ausgebucht",
+        'text' => "Nachtaktiv und günstig im Doppelpack mit Robin. "
+                . "Gutes Gehör für hohe Töne, singt im Heldenchor aber im Bass."),
+    'Flash' => array(
+        'price' => "10 €/sec",
+        'status' => "fast ausgebucht",
+        'text' => "Ihr Held für eilige Einsätze. "
+               . "Jetzt neu: Sekundengenaue Abrechnung für Ihren Auftrag."),
+    'Robin' => array(
+        'price' => "50 €/h",
+        'status' => "verfügbar",
+        'text' => "Jung und dynamisch. Guter Freund von Batman. "
+               . "Trägt gerne Strumpfhosen, vor allem im Winter."),
+);
+$hero = $heros[$name];
 
-<nav class="col-xs-3">
-    <?php include '_navi.php' ?>
-</nav>
+// Bestimmen der CSS-Klasse anhand der Helden-Verfügbarkeit
+$statusClasses = array(
+    'verfügbar' => 'success',
+    'fast ausgebucht' => 'warning',
+    'ausgebucht' => 'danger',
+);
+$class = $statusClasses[$hero['status']];
 
-<main class="col-xs-9">
-    <h1>Held <?=$name?></h1>
-    <div class="row">
-
-    <div class="col-xs-6">
-        <img src="img/<?=strtolower($name)?>.jpg" alt="<?=$name?>" class="img-rounded img-responsive">
-    </div>
-    <div class="col-xs-6">
-        <table class="table">
-            <tr><th>Name</th><td><?=$name?></td></tr>
-            <tr><th>Kosten</th><td><?=$hero['price']?></td></tr>
-            <tr><th>Status</th><td class="<?=$class?>"><?=$hero['status']?></td></tr>
-            <tr><td colspan="2"><?=$hero['text']?></td></tr>
-        </table>
-    </div>
-
-    </div>
-</main>
-
-</div>
-</body>
-</html>
+// Das vorkonfigurierte Twig nutzen, um ein Template (1. Parameter) zu rendern
+// und dabei genau zu bestimmen, welche Variablen verwendet werden sollen
+// (2. Parameter).
+echo $twig->render('held.htm.twig', array(
+    'name' => $name,
+    'hero' => $hero,
+    'statusClass' => $class,
+));
--- web/helden.php
+++ web/helden.php
@@ -1,36 +1,11 @@
-<!DOCTYPE html>
-<html lang="de">
-<head>
-    <meta charset="utf-8">
-    <title>Supero - Helden</title>
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/bootstrap-theme.min.css">
-</head>
-<body>
-<div class="container">
+<?php
+// Gemeinsame genutzte Dinge sind dorthin ausgelagert
+require_once '../bootstrap.php';
 
-<nav class="col-xs-3">
-    <?php include '_navi.php' ?>
-</nav>
+$heros = array('Batman', 'Robin', 'Flash', 'Batgirl', 'Groot', 'Joker',
+            'Supergirl', 'Superman', 'Thor');
 
-<main class="col-xs-9">
-    <h1>Supero - Helden</h1>
-    <?php
-    $heros = array('Batman', 'Robin', 'Flash', 'Batgirl', 'Groot', 'Joker',
-        'Supergirl', 'Superman', 'Thor');
-    ?>
-    <table class="table table-bordered">
-        <?php foreach($heros as $name) {
-             // im Dateisystem besser nur Kleinbuchstaben verwenden
-            $file = strtolower($name);
-            echo "<tr>";
-            echo "<td><a href='held.php?name=$name'>$name</a></td>";
-            echo "<td><img src='img/$file.jpg' alt='$name' class='img-rounded' style='width: 200px'></td>";
-            echo "</tr>";
-        } ?>
-    </table>
-</main>
-
-</div>
-</body>
-</html>
+// Das vorkonfigurierte Twig nutzen, um ein Template (1. Parameter) zu rendern
+// und dabei genau zu bestimmen, welche Variablen verwendet werden sollen
+// (2. Parameter).
+echo $twig->render('helden.htm.twig', array('heros' => $heros));
